3d850d8778ca9afe52b10970857fe4d6263b794d
CelinaJobs <celina.kinicka@gmail.com>
2023-07-22 12:56:00 +0200

    finished game

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,35 @@
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+from turtle import Turtle
+import random
+COLORS = ["black", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
 
-class CarManager:
-    pass
+
+    def new_car_creator(self):
+        random_change = random.randint(1,6)
+        if random_change == 1:
+            new_car = Turtle('square')
+            new_car.shapesize(1,2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            new_car.forward(MOVE_INCREMENT)
+            y_positions = random.randint(-250, 251)
+            new_car.goto(250, y_positions)
+            self.all_cars.append(new_car)
+
+    def move_car(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
+
+
+
+
--- player.py
+++ player.py
@@ -1,7 +1,33 @@
+from turtle import Turtle, Screen
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
+screen = Screen()
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.color('red')
+        self.setheading(90)
+        self.y_start = -280
+        self.move()
+        self.go_to_start()
 
-class Player:
-    pass
+    def move_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def move_down(self):
+        self.backward(-MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def cross_the_road(self):
+       if self.ycor() > FINISH_LINE_Y:
+           return True
+       else: return False
+
+
