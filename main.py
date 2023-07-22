e48118060283607615b5a8f9f517b13f177aa916
CelinaJobs <celina.kinicka@gmail.com>
2023-07-22 12:56:00 +0200

    finished game

--- main.py
+++ main.py
@@ -5,10 +5,38 @@ from car_manager import CarManager
 from scoreboard import Scoreboard
 
 screen = Screen()
+screen.title("Celiny gra w samochodziki")
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+me = Player()
+car = CarManager()
+score = Scoreboard()
+
+screen.listen()
+screen.onkey(me.move_up, "Up")
+screen.onkey(me.move_down, "Down")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car.new_car_creator()
+    car.move_car()
+
+    # detect collision with a car
+    for car in car.all_cars:
+        if car.distance(me) < 20:
+            game_is_on = False
+            score.game_over()
+
+    # detect crossing the screen
+    if me.cross_the_road():
+        me.go_to_start()
+        car.level_up()
+        score.increase_level()
+
+
+
+screen.exitonclick()
+
